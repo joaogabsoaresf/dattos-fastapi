63b8cd5d21baae6aaa2bf39c0a2e2f163cbcafa9
joaogabsoaresf <jgsoares.francisco@gmail.com>
2024-08-23 00:27:45 -0300

    fix: add body handler

--- format_request.py
+++ format_request.py
@@ -1,12 +1,14 @@
+from datetime import datetime
+
 class FormatMessage:
     def __init__(self, data) -> None:
-        self.contact_name = data.get('chatName')
-        self.user_name = data.get('senderName')
-        self.client_phone = data.get('phone')
-        self.owner_phone = data.get('connectedPhone')
-        self.is_group = data.get('isGroup')
-        self.from_client = not data.get('fromMe')
-        self.message_time = data.get('momment')
+        self.contact_name = data.get('chatName')if data.get('chatName') else '?'
+        self.user_name = data.get('senderName') if data.get('senderName') else '?'
+        self.client_phone = data.get('phone') if data.get('phone') else '?'
+        self.owner_phone = data.get('connectedPhone') if data.get('connectedPhone') else '?'
+        self.is_group = data.get('isGroup') if data.get('isGroup') != None else False
+        self.from_client = not data.get('fromMe') if data.get('fromMe') != None else False
+        self.message_time = data.get('momment') if data.get('momment') else datetime.now()
         
     def message_fields(self):
         return {
